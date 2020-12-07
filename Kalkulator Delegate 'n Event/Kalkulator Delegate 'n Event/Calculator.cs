7eb7ce80fcb451664fe1c146de399a04b4cbb591
yunita.nurisfa <62230041+yunitanurisfa@users.noreply.github.com>
2020-12-07 19:16:47 +0700

    Add files via upload

---
+++ Kalkulator Delegate 'n Event/Kalkulator Delegate 'n Event/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator_Delegate__n_Event
+{
+    class Calculator
+    {
+       
+        public double Penambahan(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Pengurangan(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Perkalian(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Pembagian(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Kalkulator Delegate 'n Event/Kalkulator Delegate 'n Event/Proses Kalkulator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulator_Delegate__n_Event
+{
+    public partial class Proses_Kalkulator : Form
+    {
+        public delegate void CalculatorEventHandler(double index, double a, double b);
+        public event CalculatorEventHandler OnHitung;
+
+        public Proses_Kalkulator()
+            
+        {
+            InitializeComponent();
+            cmbOperasi.Items.Add("Penjumlahan");
+            cmbOperasi.Items.Add("Pengurangan");
+            cmbOperasi.Items.Add("Perkalian");
+            cmbOperasi.Items.Add("Pembagian");
+            cmbOperasi.SelectedIndex = 0;
+            cmbOperasi.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Proses_Kalkulator_Load(object sender, EventArgs e)
+        {
+          
+
+        }
+
+        private void btnProses_Click(object sender, EventArgs e)
+        {
+            double NilaiA = double.Parse(txtNilaiA.Text);
+            double NilaiB = double.Parse(txtNilaiB.Text);
+
+            if (cmbOperasi.SelectedIndex==0)
+            {
+                OnHitung(0, NilaiA, NilaiB);
+            }
+            else if (cmbOperasi.SelectedIndex==1)
+            {
+                OnHitung(1, NilaiA, NilaiB);
+            }
+            else if (cmbOperasi.SelectedIndex==2)
+            {
+                OnHitung(2, NilaiA, NilaiB);
+            }
+            else 
+            {
+                OnHitung(3, NilaiA, NilaiB);
+            }
+        }
+    }
+}
